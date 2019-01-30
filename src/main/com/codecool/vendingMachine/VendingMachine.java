7e1bc7f8c6f7c05e4f977892236c7d03a420ec20
bogdanszonja <bogdanszonja@gmail.com>
2019-01-30 16:40:12 +0100

    test case for coin initialization added

--- src/main/com/codecool/vendingMachine/VendingMachine.java
+++ src/main/com/codecool/vendingMachine/VendingMachine.java
@@ -1,5 +1,12 @@
 package main.com.codecool.vendingMachine;
 
+import java.util.Map;
+
 public class VendingMachine {
+
+	public Map<Coin, Integer> getCoinInventory() {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 
 }
--- src/test/com/codecool/vendingMachine/VendingMachineTest.java
+++ src/test/com/codecool/vendingMachine/VendingMachineTest.java
@@ -1,5 +1,36 @@
 package test.com.codecool.vendingMachine;
 
+import main.com.codecool.vendingMachine.VendingMachine;
+import main.com.codecool.vendingMachine.Coin;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VendingMachineTest {
+	
+	private VendingMachine vendingMachine;
+	
+	@BeforeEach
+	void setup() {
+		this.vendingMachine = new VendingMachine();
+	}
+	
+	@Test
+	void testInitializationForCoins() {
+		Map<Coin, Integer> expectedCoinInventory = new HashMap<Coin, Integer>();
+		
+		expectedCoinInventory.put(Coin.PENNY, 10);
+		expectedCoinInventory.put(Coin.NICKEL, 10);
+		expectedCoinInventory.put(Coin.DIME, 5);
+		expectedCoinInventory.put(Coin.QUARTER, 5);
+		
+		Map<Coin, Integer> result = this.vendingMachine.getCoinInventory();
+		
+		assertEquals(expectedCoinInventory, result);
+		}
 
 }
