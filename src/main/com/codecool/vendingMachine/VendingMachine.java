8cc9f995136207d89ac6a9c88d622c001ac735bd
bogdanszonja <bogdanszonja@gmail.com>
2019-01-30 21:42:00 +0100

    coin initialization added

--- src/main/com/codecool/vendingMachine/VendingMachine.java
+++ src/main/com/codecool/vendingMachine/VendingMachine.java
@@ -1,12 +1,25 @@
 package main.com.codecool.vendingMachine;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class VendingMachine {
+	
+	private Map<Coin, Integer> coinInventory = new HashMap<Coin, Integer>();
+	
+	public VendingMachine() {
+		this.initialize();
+	}
+
+	private void initialize() {
+		this.coinInventory.put(Coin.PENNY, 10);
+		this.coinInventory.put(Coin.NICKEL, 10);
+		this.coinInventory.put(Coin.DIME, 5);
+		this.coinInventory.put(Coin.QUARTER, 5);	
+	}
 
 	public Map<Coin, Integer> getCoinInventory() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.coinInventory;
 	}	
 
 }
