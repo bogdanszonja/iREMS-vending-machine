3b4fc9bbfc06c0c606e0939db2e378b485ad2e2c
bogdanszonja <bogdanszonja@gmail.com>
2019-01-31 10:36:50 +0100

    Inventory class using generic type for coins and products

---
+++ src/main/com/codecool/vendingMachine/Inventory.java
@@ -0,0 +1,31 @@
+package main.com.codecool.vendingMachine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inventory<T> {
+
+	private Map<T, Integer> inventory = new HashMap<T, Integer>();
+	
+	public void add(T item) {
+		int count = this.inventory.get(item);
+		this.inventory.put(item, count + 1);
+	}
+	
+	public void remove(T item) {
+		int count = this.inventory.get(item);
+		this.inventory.put(item, count - 1);
+	}
+	
+	public boolean hasItem(T item) {
+		return this.inventory.get(item) > 0 ? true : false;
+	}
+	
+	public void putAmount(T item, int amount) {
+		this.inventory.put(item, amount);
+	}
+	
+	public Map<T, Integer> getInventory() {
+		return this.inventory;
+	}
+}
